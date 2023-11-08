4458d86b454cd1da04fdf0e4157ccf11d6215029
Semdelion <semdelion@gmail.com>
2023-11-08 20:35:57 +0700

    foreground service

--- app/src/main/java/com/semdelion/presentation/ui/tabs/dashboard/services/ForegroundService.kt
+++ app/src/main/java/com/semdelion/presentation/ui/tabs/dashboard/services/ForegroundService.kt
@@ -1,13 +1,22 @@
 package com.semdelion.presentation.ui.tabs.dashboard.services
 
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.semdelion.presentation.R
+import com.semdelion.presentation.RoutingActivity
 import java.util.concurrent.Executors
 
 private const val NOTIFICATION_ID = 1
+private const val CHANNEL_ID = "ForegroundService_channel"
 
 class ForegroundService: Service() {
 
@@ -22,24 +31,64 @@ class ForegroundService: Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-//TODO
-       /* startForeground(
-            NOTIFICATION_ID,
+        createNotificationChannel()
 
-        )*/
-        Log.i("Semdelion", "ForegroundService onStartCommand")
+        val pendingIntent: PendingIntent =
+            Intent(this, RoutingActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(this, 0, notificationIntent,
+                    PendingIntent.FLAG_IMMUTABLE)
+            }
+
+        startForeground(
+            NOTIFICATION_ID,
+            createNotification(this,
+                "Foreground service",
+                "Starting...",
+                R.drawable.ic_add_favorite,
+                pendingIntent
+            )
+        )
+
         Executors.newSingleThreadExecutor().execute {
             fileUpLoader.upload(
                 object : ProgressCallBack {
                     override fun onProgress(progress: Int) {
-                        Log.i("Semdelion", "${Thread.currentThread().name} Progress $progress%")
+                        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                        notificationManager.notify(
+                            NOTIFICATION_ID,
+                            createNotification(this@ForegroundService,
+                                "Foreground service - started",
+                                "Progress $progress%",
+                                R.drawable.ic_add_favorite,
+                                pendingIntent)
+                        )
                     }
                 }
             )
             stopSelf()
         }
-
         return START_NOT_STICKY
+    }
+
+    private fun createNotification(context: Context, title: String, contentText: String, icon: Int, pendingIntent: PendingIntent): Notification {
+        return NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(contentText)
+            .setSmallIcon(icon)
+            .setContentIntent(pendingIntent)
+            .setSilent(true)
+            .build()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(CHANNEL_ID, "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_MIN)
+            serviceChannel.enableVibration(false)
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager!!.createNotificationChannel(serviceChannel)
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/semdelion/presentation/ui/tabs/dashboard/services/ServicesFragment.kt
+++ app/src/main/java/com/semdelion/presentation/ui/tabs/dashboard/services/ServicesFragment.kt
@@ -1,21 +1,47 @@
 package com.semdelion.presentation.ui.tabs.dashboard.services
 
+import android.Manifest
+import android.content.ComponentName
+import android.content.Context.BIND_AUTO_CREATE
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.semdelion.presentation.R
 import com.semdelion.presentation.core.views.BaseFragment
 import com.semdelion.presentation.core.views.factories.viewModel
 import com.semdelion.presentation.databinding.FragmentServicesBinding
+import kotlinx.coroutines.launch
 
 class ServicesFragment : BaseFragment() {
     private lateinit var binding: FragmentServicesBinding
     override val viewModel by viewModel<ServicesViewModel>()
+
+    /*private val connectivity = object: ServiceConnection {
+
+        override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
+            val uploadBinder = (service as BoundService.UploadBinder)
+            uploadBinder.subscribeToProgress {progress ->
+                requireActivity().runOnUiThread {
+                                binding.boundProgressText.text = "Bound service progress ${progress}%"
+                }
+            }
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+        }
+
+    }*/
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
@@ -38,6 +64,20 @@ class ServicesFragment : BaseFragment() {
                 activity?.startForegroundService(it)
             }
         }
+
+       /* binding.boundButton.setOnClickListener {
+            Intent(this.context, BoundService::class.java).also {
+                activity?.startService(it)
+            }
+        }
+
+
+
+        Intent(this.context, BoundService::class.java).also {
+            requireActivity().bindService(it, connectivity, BIND_AUTO_CREATE)
+        }*/
+
+        ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0)
         return binding.root
     }
 }
